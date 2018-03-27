ce944075603ed9037a2bfabef7f2ed5277dff83a
Zimaxim <zimax@bk.ru>
2018-03-27 10:55:24 +0300

    Add files via upload

--- less04/tree.js
+++ less04/tree.js
@@ -12,68 +12,68 @@
 var fs = require('fs');
 var path = require('path');
 
-var aDIRs = function (){
+var aDIRs = function () {
   var folders = [],
-      files = [];
-  function addDir( arr ){
+    files = [];
+  function addDir(arr) {
     folders.push(arr);
   }
-  function addFile( arr ){
+  function addFile(arr) {
     files.push(arr);
   }
   return {
-    arrFolders : folders,
-    arrFiles : files,
-    pushDir : function(arr) { addDir(arr); },
-    pushFile : function(arr){ addFile(arr);}
+    arrFolders: folders,
+    arrFiles: files,
+    pushDir: function (arr) { addDir(arr); },
+    pushFile: function (arr) { addFile(arr); }
   }
 }
 var aPaths = new aDIRs();
 
 
-var cbFnc = function(err, result){
-    if(err) console.log("ERROR: %o", err);
+var cbFnc = function (err, result) {
+  if (err) console.log("ERROR: %o", err);
 
-    console.log( '\n Folders:' );
-    console.log( aPaths.arrFolders);
+  console.log('\n Folders:');
+  console.log(aPaths.arrFolders);
 
-    console.log( '\n Files:' );
-    console.log( aPaths.arrFiles);
+  console.log('\n Files:');
+  console.log(aPaths.arrFiles);
 }
 
 
 
-var tree = function(dir, callbackFnc) {
+var tree = function (dir, callbackFnc) {
   var results = [];
-  
 
 
-  callbackFnc = callbackFnc || cbFnc;  
 
-  
+  callbackFnc = callbackFnc || cbFnc;
 
 
-  fs.readdir(dir, function(err, list) {
+
+
+  fs.readdir(dir, function (err, list) {
     // 
-	if (err) return  callbackFnc(err);
-  
-  //console.log("callbackFnc = %s", callbackFnc);
+    if (err) return callbackFnc(err);
 
-	// Подсчет елементов в dir
+    //console.log("callbackFnc = %s", callbackFnc);
+
+    // Подсчет елементов в dir
     var pending = list.length;
-	
+
     if (!pending) return callbackFnc(null, results);
-	
-    list.forEach(function(file) {
+
+    list.forEach(function (file) {
       file = path.resolve(dir, file);    // dir +"/"+file
-	  
-      fs.stat(file, function(err, stat) {
+
+      fs.stat(file, function (err, stat) {
         if (stat && stat.isDirectory()) {
           //console.log("dir = %s", file);
-          
+
           aPaths.pushDir(file);
 
-          tree(file, function(err, res) {
+          tree(file, function (err, res) {
             results = results.concat(res);
             //console.log("pending= %s lenght = %s", pending, results.length) ;
             if (!--pending) callbackFnc(null, results);
@@ -83,7 +83,7 @@ var tree = function(dir, callbackFnc) {
           results.push(file);
           aPaths.pushFile(file);
           //console.log("pending= %s lenght = %s", pending, results.length) ;
-          
+
           if (!--pending) callbackFnc(null, results);
         }
       });
@@ -97,14 +97,14 @@ console.log('__dirname= %o', __dirname);
 //console.log('arguments: ');
 process.argv.forEach(function (val, index, array) {
   // console.log(index + ': ' + val);
-  if ( index == 2 ){ 
-       rootDir = val;
-       console.log('rootDir : ' + val);
-   }
+  if (index == 2) {
+    rootDir = val;
+    console.log('rootDir : ' + val);
+  }
 });
 
 
-tree( rootDir, cbFnc);
+tree(rootDir, cbFnc);
 
 
 
