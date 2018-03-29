afd6e5e17d77a26ccaaf9915c5e8993ae3d3ec56
Zimaxim <zimax@bk.ru>
2018-03-29 18:03:16 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+
+'use strict';
+const Reader = require('./reader.js');
+const Writer = require('./writer.js');
+const Transformer = require('./transformer.js');
+
+
+
+let array_of_data = ['1', '2', '3', '4', '5'];
+
+//let  array_of_data = [1, 2, 3, 4, 5];
+let r_opts = { objectMode: true };
+let w_opts = {
+    objectMode: true
+    , highWaterMark: 1     //ограничем буфер; при таком маленьком значении каждый раз будет вызываться событие 'drain'
+};
+let t_opts = {
+    readableObjectMode: true //читать из потока Transform будут объекты
+    , writableObjectMode: false //записывать в поток Transform можно либо строки или буфер
+    , decodeStrings: false
+};
+
+const R = new Reader(array_of_data, r_opts);
+const W = new Writer(w_opts);
+const T = new Transformer(t_opts);
+
+
+function onDrain() {
+    console.log('W in flowing mode'); //, R._readableState.flowing);
+    T.resume();
+}
+
+
+function drainSwitch(data, cb) {
+    console.log('drainSwitch :: data : %s ', data);
+
+    if (!W.write(data)) {
+        T.pause();
+        W.once('drain', cb);
+    } else {
+        process.nextTick(cb);
+    }
+}
+
+R.pipe(T).pipe(W);
