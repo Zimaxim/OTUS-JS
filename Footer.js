660361b0a06b883a773f847bbdc98436a990991d
Zimaxim <zimax@bk.ru>
2018-04-15 15:44:19 +0300

    Add files via upload

---
+++ Footer.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class Footer extends Component {
+  render() {
+    return (
+       <footer className="App-footer">
+			<h5>Copyright OTUS (C) 2018</h5>
+		</footer>
+    );
+  }
+}
+
+export default Footer;
